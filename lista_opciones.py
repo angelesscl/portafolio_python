e84e98ee06a21b762fc0005aad9689f49818a42f
angelesscl <agmerida@scl.edu.gt>
2024-06-04 13:46:02 -0600

    martes_4

--- lista_opciones.py
+++ lista_opciones.py
@@ -14,14 +14,14 @@ lista_de_opciones = [
 
 # Función
 def cargar_opciones():
-    print("-----------------------------")
-    columna = 0
+    print("-"*30)
+
     for opcion in lista_de_opciones:
         indice = lista_de_opciones.index(opcion)+1
-        if columna < 1:
-            print(f"[{indice}]{opcion}     ",end="")
-            columna +=1
+
+        if indice%2 == 0:
+            print(f"[{indice}] {opcion        }")
         else:
-            print(f"[{indice}]{opcion}    ")
-            columna = 0
-    print("\n----------------------------")
+            mensaje = f"[{indice}] {opcion}"
+            print(mensaje, " "*(15-len(mensaje)), end="")
+    print("\n","-"*29)
